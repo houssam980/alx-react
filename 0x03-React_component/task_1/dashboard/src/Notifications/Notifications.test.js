2646d3be85b1a4b783618d8e4e2be862865a68b0
houssam980 <houssam.ghmari1243@gmail.com>
2024-07-04 05:05:36 -0700

    test

--- 0x03-React_component/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Notifications from "./Notifications";
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
 
-describe("Notification component tests", () => {
-  it("renders Notification component without crashing", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification).toBeDefined();
+describe('<NotificationItem />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<NotificationItem />);
+    shallow(<NotificationItem />);
   });
 
-  it("renders ul", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification.find("ul")).toBeDefined();
+  it('renders type and value props', () => {
+    const wrapper = shallow(<NotificationItem type='default' value='test' />);
+    const li = wrapper.find('li');
+    expect(li).toHaveLength(1);
+    expect(li.text()).toEqual('test');
+    expect(li.prop('data-notification-type')).toEqual('default');
   });
 
-  it("renders three list items", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification.find("li")).toHaveLength(3);
-  });
-
-  it("renders correct text", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification.find("p").text()).toBe("Here is the list of notifications");
+  it('renders html prop', () => {
+    const text = 'Here is the list of notifications';
+    const wrapper = shallow(
+      <NotificationItem html={{ __html: '<u>test</u>' }} />
+    );
+    const li = wrapper.find('li');
+    expect(li.html()).toEqual(
+      '<li data-notification-type="default"><u>test</u></li>'
+    );
   });
 });
