5f1ce0c758aa25126f16acd6a8bffc1a3112d409
houssam980 <houssam.ghmari1243@gmail.com>
2024-07-25 07:08:23 -0700

    cm task 1

--- 0x07-react_redux_action_creator_normalizr/README.md
+++ 0x07-react_redux_action_creator_normalizr/README.md
@@ -1,1 +1,1 @@
-hey im readme file
+readme file
--- 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as notificationItem from "../../notifications.json";
-
-export const getAllNotificationsByUser = (userId) => {
-  return notificationItem.default
-    .filter((item) => item.author.id === userId)
-    .map(({ context }) => context);
-};
